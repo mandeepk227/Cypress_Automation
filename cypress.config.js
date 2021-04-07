1b7a525ce3a56c05325a379185b2f82472bdf842
Mandeep kumar <106561299+mandeepk227@users.noreply.github.com>
2021-04-06 20:00:00 -0600

    added add to cart api

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  requestTimeout:30000,
+  retries:1,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -2,12 +2,15 @@
 
 import selectors from '../Selectors/selectors.json'
 
+
 describe('My Test Suite', () => {
   const productName = "Apple MacBook Pro 13-inch"
   const invalidProductQuantity = 1
   const validProductQuantity = 2
   const errorMsg = 'The minimum quantity allowed for purchase is 2.'
   const successMsg = 'The product has been added to your shopping cart'
+
+  
 
   it('Verify the title of page', () => {
 
@@ -17,14 +20,16 @@ describe('My Test Suite', () => {
 
   it('e2e test case', () => {
     cy.visit('https://demo.nopcommerce.com');
+    
     cy.get(selectors.searchBox).type(productName);
     cy.get(selectors.wrapperMenu).click();
     cy.get(selectors.quantityInput).clear().type(invalidProductQuantity);
     cy.get(selectors.addToCartButton).click();
     cy.get(selectors.error).contains(errorMsg).should('be.visible');
     cy.get(selectors.quantityInput).clear().type(validProductQuantity);
+    cy.addproducttocart()
     cy.get(selectors.addToCartButton).click();
-    cy.get(selectors.success).contains(successMsg).should('be.visible')
+    cy.waitApiResponseStatusCode('@addproducttocart', 200)
 
   })
 
