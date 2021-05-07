a0a83e2a60f4f4959feb5e8dfc06c7d1b9d92008
Mandeep kumar <106561299+mandeepk227@users.noreply.github.com>
2021-05-06 20:00:00 -0600

    seperated api calls

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   requestTimeout:30000,
+  responseTimeout:30000,
+  pageLoadTimeout:60000,
+  viewportHeight:768,
+  viewportWidth:1440,
+  video: true,
   retries:1,
+  chromeWebSecurity: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
