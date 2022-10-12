8157b04fd086d32dbdd833adda4b04a079ea864c
Mandeep kumar <106561299+mandeepk227@users.noreply.github.com>
2022-10-12 11:45:37 +0530

    added file upload specfile

---
+++ cypress/e2e/fileUpload.cy.js
@@ -0,0 +1,13 @@
+describe('file upload', () =>{
+
+    const file ='img.jpg'
+
+    it('file upload verified', () =>{
+
+        cy.visit('https://cgi-lib.berkeley.edu/ex/fup.html');
+        cy.get('input[name="upfile"]').attachFile(file)
+        cy.get('[type="submit"]').click()
+        cy.get('p').eq(0).contains('You\'ve uploaded a file.')
+
+    })
+})
