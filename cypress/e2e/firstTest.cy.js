907151daaf2560862ca9a9889ea7eb7fefebc2ce
Mandeep kumar <106561299+mandeepk227@users.noreply.github.com>
2022-10-14 15:57:01 +0530

    added valid invalid test cases

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -9,28 +9,26 @@ describe('My Test Suite', () => {
   const errorMsg = 'The minimum quantity allowed for purchase is 2.'
   const successMsg = 'The product has been added to your shopping cart'
 
-  
-
-  it('Verify the title of page', () => {
-
+  beforeEach('Verify the title of page', () => {
     cy.visit('https://demo.nopcommerce.com');
     cy.title().should('eq','nopCommerce demo store' );
-  })
-
-  it('e2e test case', () => {
-    cy.visit('https://demo.nopcommerce.com');
-    
     cy.get(selectors.searchBox).type(productName);
     cy.get(selectors.wrapperMenu).click();
+  })
+
+  it('Verify error message by adding invalid product quantity ', () => {
     cy.get(selectors.quantityInput).scrollIntoView().clear().type(invalidProductQuantity);
     cy.get(selectors.addToCartButton).click();
     cy.get(selectors.error).contains(errorMsg).should('be.visible');
-    cy.get(selectors.quantityInput).clear().type(validProductQuantity);
-    cy.postApiaddproducttocart()
-    cy.get(selectors.addToCartButton).scrollIntoView()
+    cy.get('.error').find('.close').click({force:true});
+  })
+
+  it('Verify error message by adding valid product quantity ', () => {
+    cy.get(selectors.quantityInput).scrollIntoView().clear().type(validProductQuantity);
     cy.get(selectors.addToCartButton).click();
-    cy.waitApiResponseStatusCode('@postApiaddproducttocart', 200)
-    
+    cy.get('.success').should('be.visible');
+    cy.get('.flyout-cart').invoke('show').find('.cart-button').click();
+    cy.get('.product-name').should('contain', productName);
   })
 
 })
